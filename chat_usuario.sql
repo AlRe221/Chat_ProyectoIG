f75839451c77883cc1238f78abd3db46fbd9509d
Jacob <jacorez57@gmail.com>
2025-10-28 21:42:37 -0600

    Base de datos sin datos

--- chat_usuario.sql
+++ chat_usuario.sql
@@ -33,16 +33,6 @@ CREATE TABLE `grupos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `grupos`
---
-
-LOCK TABLES `grupos` WRITE;
-/*!40000 ALTER TABLE `grupos` DISABLE KEYS */;
-INSERT INTO `grupos` VALUES (1,'TheBest','2025-10-10 03:50:57',1),(2,'Sistemas','2025-10-10 03:51:19',1),(3,'Como ','2025-10-10 04:11:21',NULL);
-/*!40000 ALTER TABLE `grupos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `mensajes`
 --
 
@@ -61,18 +51,8 @@ CREATE TABLE `mensajes` (
   KEY `idx_destinatario` (`destinatario`),
   KEY `idx_grupo` (`grupo`),
   KEY `idx_fecha` (`fecha`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `mensajes`
---
-
-LOCK TABLES `mensajes` WRITE;
-/*!40000 ALTER TABLE `mensajes` DISABLE KEYS */;
-INSERT INTO `mensajes` VALUES (1,'Hola57',NULL,NULL,'Hola?','2025-10-10 03:49:43'),(2,'Jacob57',NULL,NULL,'hola','2025-10-10 04:10:53');
-/*!40000 ALTER TABLE `mensajes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `miembros_grupo`
@@ -95,16 +75,6 @@ CREATE TABLE `miembros_grupo` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `miembros_grupo`
---
-
-LOCK TABLES `miembros_grupo` WRITE;
-/*!40000 ALTER TABLE `miembros_grupo` DISABLE KEYS */;
-INSERT INTO `miembros_grupo` VALUES (1,1,NULL,'2025-10-10 03:50:57'),(2,2,NULL,'2025-10-10 03:51:19'),(3,3,8,'2025-10-10 04:11:21'),(4,3,3,'2025-10-10 04:11:21');
-/*!40000 ALTER TABLE `miembros_grupo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `usuario`
 --
 
@@ -119,16 +89,6 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Carlos','Pqnoe+r6BKGnkd5eSDbEK4ZcUr4X2uzh/FoSvXtf1p7ltXME','Hola57'),(2,'Juan','MfumQ8mj7wEsa0681QvDPwWelEdeOeTlBHlG3DDzhxuSz+1W','Carlos57'),(3,'Maria','zb3gPP/mx1gHN0yvDL1IgOm0i+9QIcWnlav425jZOPl70fj/','Mari75'),(4,'Carlos','0Ex0Mq09VTgZRyDzzoj+MYaGie9wFRf5lu/GZvOrzVzY09jj','Carlos78'),(5,'Juan','iMsIOr2pFXS2hXHocx8uB9linetbknhZZrnMTgrs9GXu1IkG','Juan78'),(6,'Maria','ShQr0soMNR8mKKl1JzQitF7ZByW5haYex2/Tma5mlL3+O8Kv','Mari78'),(7,'Maria','7Z1cdmiV4Gl3z6wLSwsNvky5ByxBOoFjY7DoRfdTy/NQ0WaW','Maria79'),(8,'Redvastion','9bg83tbJOhZjuTv1HyS+wau7f+unUVqSJ5vNYWErGnhKpraM','Jacob57'),(9,'Juan','fDPCnTutlXK6/6yHxYxHA18Y114bIaZoIVOCMpnFNMB+fs8L','Carlos578');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -139,4 +99,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-09 22:17:30
+-- Dump completed on 2025-10-28 21:37:33
